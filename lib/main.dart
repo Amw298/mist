60ac8d86305f5350a441fc57bb4c6466df9dae27
vyn203 <vyn1@pitt.edu>
2022-11-12 18:47:14 -0500

    make individual views have different themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:mist/views/store_view.dart';
+import 'package:mist/views/home_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,43 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
-        scaffoldBackgroundColor: Colors.grey.shade900,
-        backgroundColor: Colors.grey.shade900,
-        primaryColor: Colors.grey.shade900,
-        iconTheme: const IconThemeData().copyWith(color: Colors.white),
-        fontFamily: 'Montserrat',
-        textTheme: TextTheme(
-          headline2: const TextStyle(
-            color: Colors.white,
-            fontSize: 32.0,
-            fontWeight: FontWeight.bold,
-          ),
-          headline4: TextStyle(
-            fontSize: 12.0,
-            color: Colors.grey.shade500,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 2.0,
-          ),
-          bodyText1: TextStyle(
-            color: Colors.grey.shade500,
-            fontSize: 14.0,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 1.0,
-          ),
-          bodyText2: TextStyle(
-            color: Colors.grey.shade500,
-            letterSpacing: 1.0,
-          ),
-        ),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        home: const MyHomePage(title: 'Flutter Demo Home Page'));
   }
 }
 
@@ -71,19 +38,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
--- lib/views/store_view.dart
+++ lib/views/store_view.dart
@@ -10,16 +10,50 @@ class Store extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(children: [
-        Expanded(
-            child: Row(
-          children: [
-            SideBar(),
-            const Expanded(child: LibraryView(game: gameDefault))
-          ],
-        ))
-      ]),
+    return Theme(
+      data: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: Colors.grey.shade900,
+        backgroundColor: Colors.grey.shade900,
+        primaryColor: Colors.grey.shade900,
+        iconTheme: const IconThemeData().copyWith(color: Colors.white),
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          headline2: const TextStyle(
+            color: Colors.white,
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            fontSize: 12.0,
+            color: Colors.grey.shade500,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 2.0,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.grey.shade500,
+            fontSize: 14.0,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.grey.shade500,
+            letterSpacing: 1.0,
+          ),
+        ),
+      ),
+      child: Scaffold(
+        body: Column(children: [
+          Expanded(
+              child: Row(
+            children: [
+              SideBar(),
+              const Expanded(child: LibraryView(game: gameDefault))
+            ],
+          ))
+        ]),
+      ),
     );
   }
 }
