77f88929aa89b55573f58280da485540a1007ba4
vyn203 <vyn1@pitt.edu>
2022-12-13 04:12:09 -0500

    Revert "home page nav bar"
    This reverts commit 0986b956dbc9d3adeb67db882355df20b35f53b8.

--- lib/widgets/home_top_bar.dart
+++ lib/widgets/home_top_bar.dart
@@ -5,35 +5,8 @@ class HomeTopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-              decoration:
-                  BoxDecoration(border: Border(
-                    bottom: BorderSide(color: Color(0xff1681ab))
-                    ),
-                  ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  _HomeBarItem('      Sign In', LoginView()),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.10,
-                    height: MediaQuery.of(context).size.height * 0.10,
-                  ),
-                  _HomeBarItem('Register', RegisterView()),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.10,
-                    height: MediaQuery.of(context).size.height * 0.10,
-                  ),
-                  _HomeBarItem('Explore      ', Store()),
-                ],
-              )),
-        ],
-      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
     );
   }
 }
