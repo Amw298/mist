a70fecb9f4b9b5483b3643c2d573110ebc3281b3
vyn203 <vyn1@pitt.edu>
2022-12-13 03:50:25 -0500

    Revert "Merge pull request #13 from Amw298/vy"
    This reverts commit b413a443dcdc18ec3957dc49415dc67e73491b13, reversing
    changes made to e5fc89f813fc8c3a5a11b6c6e9c71496c31ad17a.

--- lib/widgets/home_top_bar.dart
+++ lib/widgets/home_top_bar.dart
@@ -5,8 +5,35 @@ class HomeTopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return Container(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+              decoration:
+                  BoxDecoration(border: Border(
+                    bottom: BorderSide(color: Color(0xff1681ab))
+                    ),
+                  ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  _HomeBarItem('      Sign In', LoginView()),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.10,
+                    height: MediaQuery.of(context).size.height * 0.10,
+                  ),
+                  _HomeBarItem('Register', RegisterView()),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.10,
+                    height: MediaQuery.of(context).size.height * 0.10,
+                  ),
+                  _HomeBarItem('Explore      ', Store()),
+                ],
+              )),
+        ],
+      ),
     );
   }
 }
