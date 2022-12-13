3c0db1750768e0b976dfababc8db522a4eae9386
vyn203 <vyn1@pitt.edu>
2022-12-13 04:41:30 -0500

    Revert "Revert "Merge pull request #13 from Amw298/vy""
    This reverts commit fd9f4388920d7ffb0a36c226085aef11a4d8561e.

--- lib/widgets/home_top_bar.dart
+++ lib/widgets/home_top_bar.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mist/views/login_view.dart';
+import 'package:mist/views/register_view.dart';
+import 'package:mist/views/store_view.dart';
+import 'package:mist/views/store_view_guest.dart';
 
 class HomeTopBar extends StatelessWidget {
   const HomeTopBar({super.key});
@@ -7,6 +11,31 @@ class HomeTopBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-    );
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+              decoration:
+                  BoxDecoration(border: Border(
+                    bottom: BorderSide(color: Color(0xff1681ab))
+                    ),
+                  ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  _HomeBarItem('      Sign In', LoginView()),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.10,
+                    height: MediaQuery.of(context).size.height * 0.10,
+                  ),
+                  _HomeBarItem('Register', RegisterView()),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.10,
+                    height: MediaQuery.of(context).size.height * 0.10,
+                  ),
+                  _HomeBarItem('Explore      ', StoreGuest()),
+                ],
+              )),
+        ],
+      );
   }
 }
